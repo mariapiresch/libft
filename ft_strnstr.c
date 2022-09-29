30ebcd8daea5c4f5f378ef40c0261e479ec06da6
María Pires Choupina <mpiressd1@gmail.com>
2022-09-30 00:57:18 +0200

    12

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: mapires- <mapires-@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 20:04:07 by mapires-          #+#    #+#             */
-/*   Updated: 2022/09/27 22:51:09 by mapires-         ###   ########.fr       */
+/*   Updated: 2022/09/30 00:51:43 by mapires-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,32 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	int		i;
-	int		j;
+	int	i;
+	int	j;
+	int	start;
 
-	if (!haystack)
+	if (len == 0 && *needle)
 		return (NULL);
+	if ((int)len == -1)
+		len = ft_strlen(haystack);
 	i = 0;
 	j = 0;
+	start = 0;
 	while (haystack[j] && needle[i] && j < (int)len)
 	{
-		if (haystack[j] == needle[i])
-		{
+		if (haystack[j++] == needle[i])
 			i++;
-			j++;
-		}
 		else if (i == 0)
 			j++;
 		else
+		{
+			j = ++start;
 			i = 0;
+		}
 	}
 	if (needle[i] == '\0')
 		return ((char *)(haystack + j - i));
-	else
-		return (NULL);
+	return (NULL);
 }
 
 /*int	main(void)
