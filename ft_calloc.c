619b73fe2934da4f53ab05ec2973aeb9da64ecf0
María Pires Choupina <mpiressd1@gmail.com>
2022-09-30 18:13:10 +0200

    calloc arreglado

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: mapires- <mapires-@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/25 11:58:39 by mapires-          #+#    #+#             */
-/*   Updated: 2022/09/25 19:29:57 by mapires-         ###   ########.fr       */
+/*   Updated: 2022/09/30 18:06:22 by mapires-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,14 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*s;
+	size_t	total;
 
-	s = (void *)malloc(count * size);
+	if (size > 4294967295 || count > 4294967295)
+		return (NULL);
+	total = size * count;
+	s = (void *)malloc(total);
 	if (!s)
 		return (NULL);
-	ft_memset(s, 0, count * size);
+	ft_memset(s, 0, total);
 	return (s);
 }
