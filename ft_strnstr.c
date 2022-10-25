f9a37efddccf4f6d404b9a85915ccd488057633e
mapires- <mapires-@student.42madrid.com>
2022-10-25 11:03:19 +0200

    corregido strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: mapires- <mapires-@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 20:04:07 by mapires-          #+#    #+#             */
-/*   Updated: 2022/09/30 12:39:42 by mapires-         ###   ########.fr       */
+/*   Updated: 2022/10/25 10:54:48 by mapires-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,34 +16,56 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	int			i;
 	int			j;
-	static int	start;
+	size_t		save;
 
 	if (len == 0 && *needle)
 		return (NULL);
-	if ((int)len == -1)
-		len = ft_strlen(haystack);
 	i = 0;
 	j = 0;
-	while (haystack[j] && needle[i] && j < (int)len)
+	save = len;
+	while (haystack[j] && needle[i] && len)
 	{
-		if (haystack[j++] == needle[i])
-			i++;
-		else if (i == 0)
-			j++;
+		if (haystack[j++] == needle[i++])
+			len--;
 		else
 		{
-			j = ++start;
+			haystack++;
+			j = 0;
 			i = 0;
+			len = --save;
 		}
 	}
 	if (needle[i] == '\0')
-		return ((char *)(haystack + j - i));
+		return ((char *)haystack);
 	return (NULL);
 }
 
+/*char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+
+	if (!*needle)
+		return ((char *)haystack);
+	if (!len)
+		return (NULL);
+	while (*haystack && len)
+	{
+		i = -1;
+		while (haystack[++i] && haystack[i] == needle[i] && len - i)
+			if (!needle[i + 1])
+				return ((char *)haystack);
+		haystack++;
+		len--;
+	}
+	return (NULL);
+}*/
+
 /*int	main(void)
 {
-	printf("%s\n", strnstr(NULL, "fake", 0));
-	printf("%s\n", ft_strnstr(NULL, "fake", 0));
+	char	haystack[30] = "aaabcabcd";
+	char needle[10] = "abcd";
+
+	printf("%s\n", strnstr(haystack, needle, 9));
+	printf("%s\n", ft_strnstr(haystack, needle, 9));
 	return (0);
 }*/
