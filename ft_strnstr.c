118d4bf511d5245ac1de52eac9f33c981a285ec0
mapires- <mapires-@student.42madrid.com>
2022-10-26 09:49:06 +0200

    finished

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: mapires- <mapires-@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/23 20:04:07 by mapires-          #+#    #+#             */
-/*   Updated: 2022/10/25 10:54:48 by mapires-         ###   ########.fr       */
+/*   Updated: 2022/10/26 09:45:36 by mapires-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,22 @@
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	int			i;
-	int			j;
 	size_t		save;
 
 	if (len == 0 && *needle)
 		return (NULL);
 	i = 0;
-	j = 0;
 	save = len;
-	while (haystack[j] && needle[i] && len)
+	while (haystack[i] && needle[i] && len)
 	{
-		if (haystack[j++] == needle[i++])
+		if (haystack[i] == needle[i])
+		{
+			i++;
 			len--;
+		}
 		else
 		{
 			haystack++;
-			j = 0;
 			i = 0;
 			len = --save;
 		}
@@ -40,32 +40,12 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	return (NULL);
 }
 
-/*char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
-{
-	size_t	i;
-
-	if (!*needle)
-		return ((char *)haystack);
-	if (!len)
-		return (NULL);
-	while (*haystack && len)
-	{
-		i = -1;
-		while (haystack[++i] && haystack[i] == needle[i] && len - i)
-			if (!needle[i + 1])
-				return ((char *)haystack);
-		haystack++;
-		len--;
-	}
-	return (NULL);
-}*/
-
 /*int	main(void)
 {
-	char	haystack[30] = "aaabcabcd";
-	char needle[10] = "abcd";
+	char	haystack[30] = "hola que tal";
+	char	needle[10] = "tal";
 
-	printf("%s\n", strnstr(haystack, needle, 9));
-	printf("%s\n", ft_strnstr(haystack, needle, 9));
+	printf("%s\n", strnstr(haystack, needle, 12));
+	printf("%s\n", ft_strnstr(haystack, needle, 12));
 	return (0);
 }*/
